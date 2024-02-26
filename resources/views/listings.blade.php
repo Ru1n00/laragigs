e50ebb2d1df0a3f75ac8414cd84794bb15b5da2b
Ru1n00 <ruhul.amin.tnt@gmail.com>
2024-02-26 17:34:11 +0600

    Change some texts

--- resources/views/listings.blade.php
+++ resources/views/listings.blade.php
@@ -1,12 +1,12 @@
 <h1>Listings</h1>
 <p>{{ $heading }}</p>
 <ul>
-    @unless(count($posts))
+    @unless(count($listings))
         <p>No posts found</p>
     @else
-        @foreach ($posts as $post)
+        @foreach ($listings as $listing)
             <li>
-                <a href="/posts/{{ $post['id'] }}">{{ $post['title'] }}</a>
+                <a href="/listings/{{ $listing['id'] }}">{{ $listing['title'] }}</a>
             </li>
         @endforeach
     @endunless
