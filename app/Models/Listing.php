806102a89ee839af31dd242398743e83351db30f
Ru1n00 <ruhul.amin.tnt@gmail.com>
2024-02-28 09:10:43 +0600

    Update Listing model and create ListingSeede and ListingFactory

--- app/Models/Listing.php
+++ app/Models/Listing.php
@@ -2,35 +2,10 @@
 
 namespace App\Models;
 
-class Listing {
-    public static function all() {
-        return [
-            [
-                'id' => 1,
-                'title' => 'My first post',
-                'content' => 'This is my first post'
-            ],
-            [
-                'id' => 2,
-                'title' => 'My second post',
-                'content' => 'This is my second post'
-            ],
-            [
-                'id' => 3,
-                'title' => 'My third post',
-                'content' => 'This is my third post'
-            ]
-        ];
-    }
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
 
-    public static function find($id) {
-        $allListings = self::all();
-
-        forEach($allListings as $listing) {
-            if($listing['id'] == $id) {
-                return $listing;
-            }
-        }
-        return [];
-    }
+class Listing extends Model
+{
+    use HasFactory;
 }
